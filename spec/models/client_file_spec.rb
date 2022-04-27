b2840104acf60cdbef7ac79b4c88a9eca10e65b9
manuel1280 <komodocr7@gmail.com>
2022-04-27 12:16:37 -0500

    create initial tests including files

--- spec/models/client_file_spec.rb
+++ spec/models/client_file_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe ClientFile, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    file = ActiveStorage::Blob.create_after_upload!(
+        io: File.open(Rails.root.join('spec', 'files', 'words_test.txt'), 'rb'),
+        filename: 'words_test.txt',
+        content_type: 'text/plain'
+    )
+    ClientFile.create!(file: file)
+  end
+
+  describe "creation" do
+    it "should have one item created" do
+      expect(ClientFile.all.count).to eq(1)
+    end
+  end
+
+  describe "file_attached" do
+    it "should have a file attached" do
+      expect(ClientFile.last.file.attached?).to be true
+    end
+  end
 end
